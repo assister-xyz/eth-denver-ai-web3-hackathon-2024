2d9c5776f16eb0e2c61d29f6c7e8c9f9a0c06382
MarianPetlovanyi <petlyovanyy@gmail.com>
2024-03-11 13:19:00 +0200

    Fixed error with url

--- frontend/config.py
+++ frontend/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BACKEND_URL = os.getenv("BACKEND_URL")
-CONTRIBUTOR_URL = os.getenv("CONTRIBUTOR_URL")
+CONTRIBUTOR_URL = os.getenv("CONTRIBUTOR_URL", "")
